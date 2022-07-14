ff70e77966ff0f034f2ac5ffdf3519b0a3eaf2d8
Carter Brown <carter.b@earthlink.net>
2022-07-13 20:10:32 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -3,5 +3,15 @@ using System.Collections.Generic;
 
 namespace cse210_02
 {
-    
+    class program
+    {
+        // Starts game
+        static int Main()
+        {
+            PlayGame playGame = new PlayGame();
+            // Will be making a start game in PlayGame class
+            playGame.StartGame();
+            return 0;
+        }
+    }
 }
