b045e51ee2048285736717fa7fffd968a79acfba
Carter Brown <carter.b@earthlink.net>
2022-07-13 20:20:27 -0600

    commit

--- PlayGame.cs
+++ PlayGame.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace cse210_02
 {
     public class PlayGame
     {
-        
+        List<Card> card = new List<Card>();
+        bool isPlaying = true;
+        int winningbounus = 100;
+        int losingloss = 75;
+        int totalScore = 300;
+        int currentCard;
+        int nextCard;
+        public PlayGame()
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                Card card = new Card();
+                
+            }
+        }
     }
 }
